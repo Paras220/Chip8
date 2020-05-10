d9ba0e5c092fbbfc90e093cb26678ac89d159eea
paras220 <paras.c@live.com>
2020-05-10 02:45:19 -0400

    Basic structure of the emulator

---
+++ src/Core/Chip8.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+
+// TODO
+// Opcodes
+// SDL2 output
+
+const int PC_START = 0x200;
+
+class Chip8 {
+public:
+    Chip8();
+	void Cycle();
+	bool LoadROM(const char* filename);
+
+    std::array<uint8_t, 64 * 32> display = {};
+
+private:
+    uint16_t pc;
+    uint16_t opcode;
+    uint16_t registerI;
+
+    std::array<uint8_t, 4096> memory = {};
+    std::array<uint8_t, 16> V = {};
+    std::array<uint8_t, 16> stack = {};
+    uint8_t sp;
+
+    uint8_t delayTimer;
+    uint8_t soundTimer;
+};
+
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "Core/Chip8.h"
+
+
+int main(int argc, char *argv[])
+{
+	Chip8 c8;
+
+	if (!c8.LoadROM("invaders.c8")) {
+		std::cout << "Could not load the ROM. Exiting program";
+		return -1;
+	}
+}
